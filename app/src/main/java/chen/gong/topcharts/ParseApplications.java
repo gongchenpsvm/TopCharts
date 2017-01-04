7bb599a2650ef41b411d4d9f9daff04003bdd624
gongchen <gongchen@usc.edu>
2017-01-03 22:58:00 -0800

    Extract each entry of xml into a FeedEntry object, which has String member variables to store various information of each app

--- app/src/main/java/chen/gong/topcharts/ParseApplications.java
+++ app/src/main/java/chen/gong/topcharts/ParseApplications.java
@@ -1,5 +1,7 @@
 package chen.gong.topcharts;
 
+import android.util.Log;
+
 import org.xmlpull.v1.XmlPullParser;
 import org.xmlpull.v1.XmlPullParserFactory;
 
@@ -23,7 +25,7 @@ public class ParseApplications {
 
     public boolean parse(String xmlData){
         boolean status = true;
-        FeedEntry currentRecord;
+        FeedEntry currentRecord = null;
         boolean inEntry = false;
         String textValue = "";
         try {
@@ -32,9 +34,47 @@ public class ParseApplications {
             XmlPullParser xpp = factory.newPullParser();
             xpp.setInput(new StringReader(xmlData));
             int eventType = xpp.getEventType();
-            while(eventType != XmlPullParser.END_DOCUMENT){
-                 //Extract
+            while(eventType != XmlPullParser.END_DOCUMENT){//Extract
+                String tagName = xpp.getName();//tagName can be null, if parser not in a tag,
+                switch (eventType){
+                    case XmlPullParser.START_TAG:
+                        Log.d(TAG, "parse: Starting tag for " + tagName);
+                        if("entry".equalsIgnoreCase(tagName)){
+                            inEntry = true;
+                            currentRecord = new FeedEntry();
+                        }
+                        break;
+                    case XmlPullParser.TEXT://Data is available
+                        textValue = xpp.getText();
+                        break;
+                    case XmlPullParser.END_TAG:
+                        Log.d(TAG, "parse: Starting tag for " + tagName);
+                        if(inEntry) {//Check pullParser is inside an entry tag
+                            if ("entry".equalsIgnoreCase(tagName)) {//Call method on String
+                                applications.add(currentRecord);
+                                inEntry = false;//Reach end tag of an entry
+                            } else if ("name".equalsIgnoreCase(tagName)) {
+                                currentRecord.setName(textValue);
+                            } else if ("artist".equalsIgnoreCase(tagName)) {
+                                currentRecord.setArtist(textValue);
+                            } else if ("releaseDate".equalsIgnoreCase(tagName)) {
+                                currentRecord.setReleaseDate(textValue);
+                            } else if ("summary".equalsIgnoreCase(tagName)) {
+                                currentRecord.setSummary(textValue);
+                            } else if ("image".equalsIgnoreCase(tagName)) {
+                                currentRecord.setImageURL(textValue);
+                            }
+                        }
+                        break;
+                    default://Nothing to do
+                }
+                eventType = xpp.next();
             }
+            for (FeedEntry app : applications){
+                Log.d(TAG, "************");
+                Log.d(TAG,app.toString());
+            }
+
         }catch (Exception e){
             status = false;
             e.printStackTrace();
